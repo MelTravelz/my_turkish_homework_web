e42333334d9884c7681354ac22c29799361ba61b
Melony Franchini <meltravelz@gmail.com>
2023-04-10 00:04:31 -0600

    Create new homework view page - need to build form

--- spec/features/users/pages/new_spec.rb
+++ spec/features/users/pages/new_spec.rb
@@ -12,9 +12,11 @@ RSpec.describe "/users/:id/pages/new" do
 
     it "displays a random image, random turkish verb, & form with 10 text fields" do
       expect(page).to have_content("My Turkish Homework")
+      expect(page).to have_content("Turkish Verb:")
       expect(page).to have_content("(i) bilmek")
+      expect(page).to have_content("Theme Image:")
+      expect(page).to have_css("img[src^='https://images.unsplash.com/photo-1680792563719-288027b2a090?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MzM0NTl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODExMDYzNDA&ixlib=rb-4.0.3&q=80&w=400']")
+      save_and_open_page
     end
-
-
   end
 end
